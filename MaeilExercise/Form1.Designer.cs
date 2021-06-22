a81a95bc4a69d12b676dabbccbd45f7f35088d46
Fabio <faac@ua.pt>
2021-06-22 19:10:18 +0100

    quase completo

---
+++ MaeilExercise/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace MaeilExercise
+{
+    partial class btn_saveChanges
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_loadFile = new System.Windows.Forms.Button();
+            this.btn_saveFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.title = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label_error = new System.Windows.Forms.Label();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_last = new System.Windows.Forms.Button();
+            this.btn_first = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_loadFile
+            // 
+            this.btn_loadFile.Location = new System.Drawing.Point(12, 46);
+            this.btn_loadFile.Name = "btn_loadFile";
+            this.btn_loadFile.Size = new System.Drawing.Size(106, 40);
+            this.btn_loadFile.TabIndex = 0;
+            this.btn_loadFile.Text = "Load File";
+            this.btn_loadFile.UseVisualStyleBackColor = true;
+            this.btn_loadFile.Click += new System.EventHandler(this.btn_loadFile_Click);
+            // 
+            // btn_saveFile
+            // 
+            this.btn_saveFile.Location = new System.Drawing.Point(124, 46);
+            this.btn_saveFile.Name = "btn_saveFile";
+            this.btn_saveFile.Size = new System.Drawing.Size(106, 40);
+            this.btn_saveFile.TabIndex = 1;
+            this.btn_saveFile.Text = "Save File";
+            this.btn_saveFile.UseVisualStyleBackColor = true;
+            this.btn_saveFile.Click += new System.EventHandler(this.btn_saveFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1363, 510);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(520, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(430, 32);
+            this.title.TabIndex = 4;
+            this.title.Text = "Program to Load and Edit Excel Files";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label_error
+            // 
+            this.label_error.AutoSize = true;
+            this.label_error.ForeColor = System.Drawing.Color.Red;
+            this.label_error.Location = new System.Drawing.Point(466, 66);
+            this.label_error.Name = "label_error";
+            this.label_error.Size = new System.Drawing.Size(0, 20);
+            this.label_error.TabIndex = 5;
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(573, 667);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(104, 44);
+            this.btn_prev.TabIndex = 6;
+            this.btn_prev.Text = "Previous";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(694, 667);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(115, 44);
+            this.btn_next.TabIndex = 7;
+            this.btn_next.Text = "Next";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(1181, 667);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(218, 44);
+            this.btn_delete.TabIndex = 8;
+            this.btn_delete.Text = "Delete Selected Rows";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_last
+            // 
+            this.btn_last.Location = new System.Drawing.Point(835, 667);
+            this.btn_last.Name = "btn_last";
+            this.btn_last.Size = new System.Drawing.Size(115, 44);
+            this.btn_last.TabIndex = 9;
+            this.btn_last.Text = "Last";
+            this.btn_last.UseVisualStyleBackColor = true;
+            this.btn_last.Click += new System.EventHandler(this.btn_last_Click);
+            // 
+            // btn_first
+            // 
+            this.btn_first.Location = new System.Drawing.Point(435, 667);
+            this.btn_first.Name = "btn_first";
+            this.btn_first.Size = new System.Drawing.Size(115, 44);
+            this.btn_first.TabIndex = 10;
+            this.btn_first.Text = "First";
+            this.btn_first.UseVisualStyleBackColor = true;
+            this.btn_first.Click += new System.EventHandler(this.btn_first_Click);
+            // 
+            // btn_saveChanges
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 745);
+            this.Controls.Add(this.btn_first);
+            this.Controls.Add(this.btn_last);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.label_error);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_saveFile);
+            this.Controls.Add(this.btn_loadFile);
+            this.Name = "btn_saveChanges";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_loadFile;
+        private System.Windows.Forms.Button btn_saveFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label_error;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_last;
+        private System.Windows.Forms.Button btn_first;
+    }
+}
+
---
+++ MaeilExercise/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OfficeOpenXml;
+using ClosedXML.Excel;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+using System.Collections;
+
+namespace MaeilExercise
+{
+    public partial class btn_saveChanges : Form
+    {
+
+        //#################################### Global Variables  ###########################################
+        Dictionary<String, String[]> rows = new Dictionary<string, string[]>(); // key(rowId) : value(all cells values) 
+        ArrayList logs = new ArrayList();
+        DataTable dt = new DataTable();
+        String filePath;
+        int pageNumber = 0;
+        int pageSize = 20 ;
+        int maxRowId = 0;
+        int numOfColumns = 0;
+
+
+        public btn_saveChanges()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //#################################### Event Listeners  ###########################################
+
+        private void btn_loadFile_Click(object sender, EventArgs e)
+        {
+            // Choose file
+            openFileDialog1.ShowDialog();
+            filePath = openFileDialog1.FileName;
+            System.Diagnostics.Debug.WriteLine("File Opened: " + filePath);
+            label_error.Text = "Loading file...Please wait.";
+
+            // Check Extension and load file
+            string fileExt = Path.GetExtension(filePath);
+            if (fileExt == ".xlsx" )
+            {   
+                LoadExcel(filePath);
+                ShowRows();
+            }
+            else
+            {
+                label_error.Text = "Only .xlsx files are supported";
+            }
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            saveChanges();
+            dt.Clear();
+            pageNumber++;
+            ShowRows();
+
+        }
+
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            saveChanges();
+            dt.Clear();
+            if (pageNumber > 0)
+                pageNumber--;
+            ShowRows();
+
+        }
+
+        private void btn_first_Click(object sender, EventArgs e)
+        {
+            saveChanges();
+            dt.Clear();
+            pageNumber = 0;
+            ShowRows();
+        }
+
+        private void btn_last_Click(object sender, EventArgs e)
+        {
+            saveChanges();
+            dt.Clear();
+            pageNumber = maxRowId / pageSize;
+            ShowRows();
+
+        }
+        private void btn_saveFile_Click(object sender, EventArgs e)
+        {
+            saveExcelFile();
+            saveLogsAsync();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            deleteSelectedRows();
+        }
+
+
+        //#################################### FUNCTIONS ###########################################
+
+        private void ShowRows()
+        {
+            int start = pageNumber * pageSize;
+            for(int i = pageNumber*pageSize; i < start + pageSize;i++) { 
+                // Add the row to the rows collection.
+                if (rows.ContainsKey(i.ToString()))
+                {
+                    DataRow newRow = dt.NewRow();
+                    newRow.ItemArray = rows[i.ToString()];
+                    dt.Rows.Add(newRow);
+                }
+            }
+        }
+
+        protected void LoadExcel(string filePath)
+        {
+            // Open the Excel file
+            using (XLWorkbook workBook = new XLWorkbook(filePath))
+            {
+                // Read the first Sheet from Excel file.
+                IXLWorksheet workSheet = workBook.Worksheet(1);
+                numOfColumns = workSheet.Columns().Count() +1 ;
+
+                // Loop through the Worksheet rows.
+                bool firstRow = true;
+                foreach (IXLRow row in workSheet.Rows())
+                {
+                    // Use the first row to add columns to DataTable.
+                    if (firstRow)
+                    {
+                        dt.Columns.Add("#");
+                        foreach (IXLCell cell in row.Cells())
+                        {
+                            dt.Columns.Add(cell.Value.ToString());
+                        }
+                        firstRow = false;
+                    }
+                    else
+                    {
+                        if (!row.IsEmpty())
+                        {
+                            string[] rowValues = new string[numOfColumns];
+                            
+                            for (int i=1; i<numOfColumns; i++ )
+                                rowValues[i] = row.Cells(false).ElementAt(i-1).Value.ToString(); 
+                            
+                            string pos = maxRowId.ToString();
+                            rowValues[0] = pos;
+                            rows.Add(pos,rowValues);
+                            maxRowId++;
+                            if (maxRowId == 5000) // TODO: DELETE THIS LATER
+                                break;
+                        }
+                        
+                    }
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            // Make first column not editable
+            foreach (DataGridViewColumn dc in dataGridView1.Columns)
+            {
+                if (dc.Index.Equals(0)) {dc.ReadOnly = true;}
+            }
+            label_error.Text = "Finished Loading File!";
+
+        }
+
+        private void saveChanges()
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                string pos = dr[0].ToString();
+                string[] rowValues = new string[numOfColumns];
+                string[] tableRow = string.Join(",", dr.ItemArray).Split(',').ToArray();
+
+                // If exists, just update
+                if (rows.ContainsKey(pos))
+                {
+                    if (!rows[pos].SequenceEqual(tableRow))
+                    {
+                        string rowOldContent = string.Join(";", rows[pos][1..].ToArray());
+                        string rowNewContent = string.Join(";", tableRow[1..].ToArray());
+                        string log = DateTime.Now + "-  UPDATE:  " + "FROM - " + rowOldContent + " - TO " + rowNewContent;
+                        logs.Add(log);
+                        rows[pos] = tableRow;
+                    }
+                }
+                // Verify if it is empty. If not empty, save to map 
+               else 
+                {
+                    foreach (string s in tableRow)
+                    {
+                        if (!s.Equals(""))
+                        {
+                            // Add to Map
+                            pos = (++maxRowId).ToString();
+                            rowValues[0] = pos;
+                            for (int x = 1; x < numOfColumns; x++)
+                                rowValues[x] = tableRow[x];
+                            rows.Add(pos, rowValues);
+                            // Add log
+                            string rowContent = string.Join(";", rowValues[1..].ToArray());
+                            string log = DateTime.Now + " -  ADD:  "  + rowValues[1..];
+                            logs.Add(log);
+                            break;
+                        }
+                    }
+                       
+                }
+            }
+        }
+
+        private void deleteSelectedRows()
+        {
+            foreach (DataGridViewCell oneCell in dataGridView1.SelectedCells)
+            {
+                if (oneCell.Selected)
+                {
+                    string pos = oneCell.OwningRow.Cells[0].Value.ToString();
+                    dataGridView1.Rows.RemoveAt(oneCell.RowIndex);
+                    if (rows.ContainsKey(pos))
+                    {
+                        // Add log
+                        string rowContent =  string.Join(";", rows[pos][1..].ToArray());
+                        string log = DateTime.Now + " -  DELETE:  " + rowContent;
+                        logs.Add(log);
+
+                        rows.Remove(pos);
+                    }
+                }
+            }
+        }
+
+        private async Task saveLogsAsync()
+        {
+            IEnumerable<string> xLogs =logs.Cast<string>().Select(log=>log);
+
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Logs\\" + filePath.Split("\\").Last());
+            foreach (string s in logs)
+                sw.WriteLine(s);
+            sw.Close();
+            //string path = Directory.GetCurrentDirectory() + "\\LogsFile.txt";
+            //await File.WriteAllLinesAsync(path,  xLogs);
+        }
+
+        private void saveExcelFile()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                DataTable table = new DataTable();
+
+                // New table with all columns except the 1st -> "#"
+                string[] columns = dt.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToArray();
+                for (int i = 1; i<columns.Length;  i++) 
+                    table.Columns.Add(columns[i]);
+
+                // Load all Map Values to the datatable
+                foreach (KeyValuePair<string, string[]> entry in rows)
+                {
+                    DataRow newRow = dt.NewRow();
+                    newRow.ItemArray = entry.Value[1..];
+                    dt.Rows.Add(newRow);
+                }
+
+                
+
+                // Save Datatable in Excel File
+                var worksheet = workbook.Worksheets.Add(dt, "sheetdata");
+                string path = Application.StartupPath +"\\SavedFiles\\" +  filePath.Split("\\").Last();
+                System.Diagnostics.Debug.WriteLine("Right before saving excel: " + path);
+                System.Diagnostics.Debug.WriteLine("Right before saving excel, number of rows to save : " + dt.Rows.Count);
+                workbook.SaveAs(path);
+            }
+        }
+    }
+}
